4ea777338e35629e0b8b3e7d78cd45f9852dd3cf
Cornelius Suermann <cornelius.suermann@camunda.com>
2019-07-03 08:31:58 +0200

    support GET & POST & headers
    Signed-off-by: Cornelius Suermann <cornelius.suermann@camunda.com>

--- demo/server.ts
+++ demo/server.ts
@@ -11,13 +11,26 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.post("/lambda1", (req, res) => {
-  console.log(req.body);
-  res.json({ lambda1Response: true });
+  console.log("/lambda1", req.body);
+  res.json({ exampleResponse: true });
 });
 
-app.post("/lambda2", (req, res) => {
-  console.log(req.body);
-  res.json({ lambda2Response: true });
+app.get("/getExample", (req, res) => {
+  console.log("/getExample", req.query);
+  res.json({
+    name: "John Doe",
+    address: { street: "Infinity Loop 1", zip: "12345" },
+  });
+});
+
+app.post("/postExample", (req, res) => {
+  console.log("/postExample", req.headers, req.body);
+  res.json({
+    id: 101,
+    title: "foo",
+    body: "bar",
+    userId: 1,
+  });
 });
 
 app.listen(port, () =>
